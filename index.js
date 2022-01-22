26dc12108a4b3d55681f42b3ae6bd89d3d796813
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-01-22 11:52:57 -0800

    commit tree 8a3924cd7a9350ec22c03ccaa87e9c3add38f741

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate through each char of word and create an array of these char
+  let charArr = []
+  for (i in word) {
+    charArr.push(word[i])
+  }
+  let backwordsCharArr = []
+  while (backwordsCharArr.length < word.length) {
+    backwordsCharArr.push(charArr.pop())
+  }
+  let backwordsWord = backwordsCharArr.join('')
+  return backwordsWord === word
 }
+isPalindrome('racecar')
 
 /* 
-  Add your pseudocode here
+  palindrome is a word that is spelled the same backwards and forwards
+    the backwards word array is empty originally and has the last letter of the word added first
+    if word = the backwords word then fn should return true, else return false
 */
 
 /*
-  Add written explanation of your solution here
+  .pop to return last letter of word
+  create charArr to manipulate arr backwards
+  .push to push the last char of word into the new charArr
+  create backwardsCharArr to accept the returned poped elements
+  .join backwardsCharArr to convert the array into a string seperated by nothing (not commas)
+  if word === the backwardsWord, then return true, else return false
 */
 
 // You can run `node index.js` to view these console logs
